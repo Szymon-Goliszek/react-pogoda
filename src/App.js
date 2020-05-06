f8fdaf516dc1b0b6af9e481305bc20c980f7c7e6
Szymon_Goliszek <szymon.goliszek@gmail.com>
2020-05-06 17:41:16 +0200

    Dodanie grafik

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+const Api={
+  key: "ed7d49e6acbea85dc1a3143ffab9098b",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
-    <div className="App">
-    Hello
+    <div className="app">
+    
     </div>
   );
 }
